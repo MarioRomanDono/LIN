99b460d79c81126597bfd25a2424a8b6b4423c4e
Mario Román Dono <45080792+MarioRomanDono@users.noreply.github.com>
2021-09-26 20:08:02 +0200

    Terminar parte obligatoria

--- Practica1/modlist/modlist.c
+++ Practica1/modlist/modlist.c
@@ -1,9 +1,17 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/list.h>
+#include <linux/proc_fs.h>
+#include <linux/string.h>
+#include <linux/vmalloc.h>
+#include <linux/uaccess.h>
+
 
 MODULE_LICENSE("GPL");
 
+#define BUFFER_LENGTH 256
+
+static struct proc_dir_entry *proc_entry;
 struct list_head mylist; /* Lista enlazada */
 
 /* Nodos de la lista */
@@ -13,40 +21,100 @@ struct list_item {
 };
 
 static ssize_t modlist_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
-	char kbuf[MAX_CHARS];
+	int available_space = BUFFER_LENGTH-1;
+	int numero;
+	char kbuf[BUFFER_LENGTH];
+	
+	if ((*off) > 0) /* The application can write in this entry just once !! */
+    	return 0;
+  
+	  if (len > available_space) {
+	   printk(KERN_INFO "clipboard: not enough space!!\n");
+	   return -ENOSPC;
+	  }	
+
+	
 	if (copy_from_user( &kbuf[0], buf, len ))  {
     		return -EFAULT;	
 	}
-	char comando[6];
-	int numero;
-
-	sscanf(kbuf, "%s %d", comando, numero);
-
-	if (comando == "add") {
-		struct list_item* item = vmalloc( sizeof(list_item) );
-		item->data = numero;
-		list_add_tail(item, mylist);
-	}
 	
 
-	  
+	kbuf[len] = '\0'; /* Add the `\0' */  
+	*off+=len;  
+	
 
+	if (sscanf(kbuf, "add %d", &numero) == 1) {
+		struct list_item* item = vmalloc( sizeof( struct list_item) );
+		item->data = numero;
+		list_add_tail(&item->links, &mylist);
+	} 
+	else if (sscanf(kbuf, "remove %d", &numero) == 1){
+		struct list_head *pos, *e;
+		struct list_item* item=NULL;
+		
+		list_for_each_safe(pos,e,&mylist){
+		
+		item = list_entry(pos, struct list_item, links);
+		if (item->data== numero){
+		list_del(pos);
+		vfree(item);
+		}
+		
+		}
+	} 
+	else if (strcmp(kbuf,"cleanup\0")) {
+		struct list_head *pos, *e;
+		struct list_item* item=NULL;
+		
+		list_for_each_safe(pos,e,&mylist){		
+			item = list_entry(pos, struct list_item, links);
+			list_del(pos);
+			vfree(item);
+		}
+	}
+	
+	return len;
+}
 
+static ssize_t modlist_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
+  char kbuf[BUFFER_LENGTH];
+  int nBytes = 0;
+  struct list_head *pos;
+  struct list_item* item=NULL;
+  
+  if ((*off) > 0) /* Tell the application that there is nothing left to read */
+      return 0;
+
+  list_for_each(pos,&mylist){
+  	item = list_entry(pos, struct list_item, links);
+   	nBytes += sprintf(&kbuf[nBytes],"%d\n", item->data);
+  }    
+    
+  if (len<nBytes)
+    return -ENOSPC;
+  
+    /* Transfer data from the kernel to userspace */  
+  if (copy_to_user(buf, kbuf,nBytes))
+    return -EINVAL;
+    
+  (*off)+=len;  /* Update the file pointer */
+
+  return nBytes; 
 }
 
 static const struct proc_ops proc_entry_fops = {
-    .proc_read = clipboard_read,
-    .proc_write = clipboard_write,    
+    .proc_read = modlist_read,
+    .proc_write = modlist_write,    
 };
 
 int init_modlist_module( void )
 {
   int ret = 0;
-  INIT_LIST_HEAD(mylist);
+  INIT_LIST_HEAD(&mylist);
   proc_entry = proc_create( "modlist", 0666, NULL, &proc_entry_fops);
   if (proc_entry == NULL) {
 	ret = -ENOMEM;
-	vfree(modlist);
+	vfree(&mylist);
 	printk(KERN_INFO "modlist: Can't create /proc entry\n");
   } else {
 	printk(KERN_INFO "modlist: Module loaded\n");
@@ -59,7 +127,7 @@ int init_modlist_module( void )
 void exit_modlist_module( void )
 {
   remove_proc_entry("modlist", NULL);
-  vfree(modlist);
+  vfree(&mylist);
   printk(KERN_INFO "modlist: Module unloaded.\n");
 }
 
