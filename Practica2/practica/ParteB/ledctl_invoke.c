6ceeeeabe895bae58d7b898752810cbaf0000cbc
Mario Román Dono <mario.romandono@gmail.com>
2021-10-14 00:44:07 +0200

    Arreglar implicit declaration. Falla cuando se leen valores no hex

--- Practica2/practica/ParteB/ledctl_invoke.c
+++ Practica2/practica/ParteB/ledctl_invoke.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <unistd.h>
+#include <ctype.h>
 
 #define __NR_LEDCTL 442
 
-long lin_ledctl(unsigned int mask) {
+long ledctl(unsigned int mask) {
 	return (long) syscall(__NR_LEDCTL, mask);
 }
 
@@ -14,21 +16,21 @@ int main(int argc, char* argv[]) {
 	int res;
 
 	if (argc != 2) {
-		fprintf(stderr, "Usage: ./ledctl_invoke <ledmask>");
+		fprintf(stderr, "Usage: ./ledctl_invoke <ledmask>\n");
 		return -1;
 	}
-
+       
 	mask = strtoul(argv[1], NULL, 16);
 
-	if (mask < 0x0) {
-		fprintf(stderr, "Ledmask cannot be negative");
+	if (errno != 0) {
+		fprintf(stderr, "Ledmask must be a valid hex number\n");
 		return -1;
 	}
-
-	res = lin_ledctl(mask);
+       
+	res = ledctl(mask);
 
 	if (res != 0) {
-		perror("Error during ledctl execution: ");
+		perror("Error during ledctl execution");
 		return -1;
 	}
 
