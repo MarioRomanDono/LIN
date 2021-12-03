8bb0e39556df05fd3442dcdea49688da75abc7ac
Mario Román Dono <mario.romandono@gmail.com>
2021-12-03 13:24:08 +0100

    Parte 3 terminada

--- Practica5/partes/parte3.c
+++ Practica5/partes/parte3.c
@@ -10,6 +10,7 @@
 
 #define MAX_CBUFFER_LEN 32
 #define CODESIZE 4
+#define KBUF_SIZE 256
 
 struct kfifo cbuffer;
 static struct list_head mylist;
@@ -20,44 +21,50 @@ struct list_item {
 
 struct timer_list my_timer; /* Structure that describes the kernel timer */
 struct work_struct my_work;
-static struct workqueue_struct* transfer_task; 
+static struct workqueue_struct* my_wq; 
 static struct proc_dir_entry *codetimer_entry;
+
+int espera = 0;
+struct semaphore queue;
 
 DEFINE_SEMAPHORE(mtx);
 DEFINE_SPINLOCK(sp);
 
-void noinline trace_message(char * message) {
-    asm(" ");
-}
+void noinline trace_code_in_buffer(char* random_code, int cur_buf_size) { asm(" "); };
 
-void noinline trace_workqueue(char * message) {
-    asm(" ");
-}
+void noinline trace_code_in_list(char* random_code) { asm(" "); };
+
+void noinline trace_code_read(char* random_code) { asm(" "); };
 
 static void copy_items_into_list(struct work_struct * wq) {
     char kbuf[MAX_CBUFFER_LEN];
-    int nBytes, aux, flags;
+    int nBytes, aux, i, ret;
+    unsigned long flags;
 
     spin_lock_irqsave(&sp, flags);
     nBytes = kfifo_out(&cbuffer,kbuf,MAX_CBUFFER_LEN);
-    spin_unlock_irqsave(&sp, flags);
+    spin_unlock_irqrestore(&sp, flags);
 
     aux = nBytes / CODESIZE;
 
-    for (int i = 0; i < aux; i++) {
+    ret = down_interruptible(&mtx);
+
+    for (i = 0; i < aux; i++) {
         struct list_item* item = vmalloc(sizeof(struct list_item));
         item->data = vmalloc(sizeof(char) * CODESIZE);
-        strncpy(item->data, kbuf[i * CODESIZE], CODESIZE);
+        strncpy(item->data, &kbuf[i * CODESIZE], CODESIZE);
 
-        if (down_interruptible(&mtx)) {
-            return -EINTR;
-        }
         list_add_tail(&item->links, &mylist);
-        up(&mtx);
-        trace_workqueue("Llamada a workqueue");
+
+        trace_code_in_list(item->data);
     }
 
-    printk(KERN_INFO "modlist: Elemento %d agregado\n", numero);
+    if (espera>0) {
+        up(&queue);
+        espera--;
+    }
+
+    up(&mtx);
 }
 
 /* Function invoked when timer expires (fires) */
@@ -71,8 +78,8 @@ static void fire_timer(struct timer_list *timer)
         queue_work_on(!(cpu_actual & 0x1), my_wq, &my_work); // Toma el último bit de la cpu (paridad) y lo alterna
     }
     
-    kfifo_in(&cbuffer, msg, 4);
-    trace_message(msg);
+    kfifo_in(&cbuffer, msg, CODESIZE);
+    trace_code_in_buffer(msg, CODESIZE);
 
     
     /* Re-activate the timer one second from now */
@@ -82,9 +89,16 @@ static void fire_timer(struct timer_list *timer)
 static int codetimer_open(struct inode * inode, struct file * file) {
     try_module_get(THIS_MODULE);
 
+    INIT_LIST_HEAD(&mylist);
+
+    if ( kfifo_alloc(&cbuffer, MAX_CBUFFER_LEN, GFP_KERNEL) ) {
+        printk(KERN_INFO "Can't allocate memory to fifo\n");
+        return -ENOMEM;
+    }
+
     /* Create workqueue*/
     my_wq = create_workqueue("my_queue");
-    INIT_WORK(&my_work, transfer_task);
+    INIT_WORK(&my_work, copy_items_into_list);
 
     /* Create timer */
     timer_setup(&my_timer, fire_timer, 0);
@@ -92,12 +106,61 @@ static int codetimer_open(struct inode * inode, struct file * file) {
     my_timer.expires=jiffies + HZ;  /* Activate it one second from now */
     /* Activate the timer for the first time */
     add_timer(&my_timer);
+
+    return 0;
 }
 
-static int code
+static ssize_t codetimer_read(struct file* filp, char __user* buf, size_t len, loff_t* off)  {
+    char kbuf[KBUF_SIZE];
+    int nBytes = 0;
+    struct list_head* pos, * e;
+    struct list_item* item = NULL;
+
+    if ((*off) > 0) /* Tell the application that there is nothing left to read */
+        return 0;
+
+    if (down_interruptible(&mtx)) {
+            return -EINTR;
+    }
+
+    if (list_empty(&mylist)) {
+        espera++;
+        up(&mtx);
+        if (down_interruptible(&queue)){
+            down(&mtx);
+            espera--;
+            up(&mtx);
+            return -EINTR;
+        }
+        if (down_interruptible(&mtx))
+            return -EINTR;
+    }
+
+    list_for_each_safe(pos, e, &mylist) {
+        item = list_entry(pos, struct list_item, links);
+        nBytes += sprintf(&kbuf[nBytes], "%s\n", item->data);
+        list_del(pos);
+        trace_code_read(item->data);  
+        vfree(item->data);
+        vfree(item);
+    }
+
+    up(&mtx);
+
+    if (len < nBytes)
+        return -ENOSPC;
+
+    /* Transfer data from the kernel to userspace */
+    if (copy_to_user(buf, kbuf, nBytes))
+        return -EINVAL;
+
+    (*off) += len;  /* Update the file pointer */
+
+    return nBytes;
+}
 
 static int codetimer_release(struct inode * inode, struct file * file) {
-        int flags;
+        unsigned long flags;
 
         del_timer_sync(&my_timer); 
         flush_workqueue(my_wq);
@@ -105,24 +168,24 @@ static int codetimer_release(struct inode * inode, struct file * file) {
         spin_lock_irqsave(&sp, flags);
         kfifo_reset(&cbuffer);
         kfifo_free(&cbuffer);
-        spin_unlock_irqsave(&sp, flags);
+        spin_unlock_irqrestore(&sp, flags);
 
         module_put(THIS_MODULE);
+
+        return 0;
 }
 
 static const struct proc_ops proc_entry_fops = {
     .proc_open = codetimer_open,
     .proc_release = codetimer_release,    
+    .proc_read = codetimer_read,
 };
 
 int init_timer_module( void )
 {
-    if ( kfifo_alloc(&cbuffer, MAX_CBUFFER_LEN, GFP_KERNEL) ) {
-        printk(KERN_INFO "Can't allocate memory to fifo\n");
-        return -ENOMEM;
-    }
+    sema_init(&queue, 0);
 
-    codetimer_entry = proc_create( "modlist", 0666, NULL, &proc_entry_fops);
+    codetimer_entry = proc_create( "codetimer", 0666, NULL, &proc_entry_fops);
     if (codetimer_entry == NULL) {
         kfifo_free(&cbuffer);
         printk(KERN_INFO "codetimer: Can't create /proc entry\n");
@@ -134,6 +197,7 @@ int init_timer_module( void )
 
 
 void cleanup_timer_module( void ){
+    remove_proc_entry("codetimer", NULL);
     printk(KERN_INFO "codetimer: Module unloaded\n");
 }
 
